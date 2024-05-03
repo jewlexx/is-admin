b2b97c3d53e43be4af8001c76ceb861cc04b2ca1
Juliette Cordor <professional@maybejules.com>
2024-05-04 01:30:41 +1000

    added more tests

--- src/traits/into_bytes.rs
+++ src/traits/into_bytes.rs
@@ -1,29 +1,32 @@
 //! Convert given values into their representation in memory
 
 /// Convert given values into their representation in memory
+///
+/// Note that this might not behave as expected for certain types, such as strings or heap allocated structs,
+/// as it will only return the data from the struct, not the actual data it points to
 pub trait RamBytes {
     /// Convert a given value into it's memory representation in bytes
-    fn to_ram_bytes(&self) -> &[u8];
+    ///
+    /// # Safety
+    /// This function references raw memory. If this is freed, the reference will be invalid
+    unsafe fn as_ram_bytes(&self) -> &[u8];
 
     /// Convert a given value into it's memory representation in bytes
-    fn into_ram_bytes(self) -> Vec<u8>;
+    ///
+    /// # Safety
+    /// As opposed to [`as_ram_bytes`], this function clones the data into a new vector,
+    /// so it will continue to be valid even if the original data is freed
+    fn to_ram_bytes(&self) -> Vec<u8> {
+        unsafe { self.as_ram_bytes() }.to_vec()
+    }
 }
 
 impl<T> RamBytes for T {
-    fn to_ram_bytes(&self) -> &[u8] {
-        unsafe {
-            core::slice::from_raw_parts(
-                (self as *const T).cast::<u8>(),
-                core::mem::size_of_val(self),
-            )
-        }
-    }
-
-    fn into_ram_bytes(self) -> Vec<u8>
-    where
-        Self: Sized,
-    {
-        Self::to_ram_bytes(&self).to_vec()
+    unsafe fn as_ram_bytes(&self) -> &[u8] {
+        core::slice::from_raw_parts(
+            core::ptr::from_ref::<T>(self).cast::<u8>(),
+            core::mem::size_of_val(self),
+        )
     }
 }
 
@@ -31,25 +34,12 @@ impl<T> RamBytes for T {
 mod tests {
     use super::*;
 
-    fn pad_with_null<T>(vec: &mut Vec<T>, count: usize) {
-        for _ in 0..count {
-            vec.push(unsafe { std::mem::zeroed() });
-        }
-    }
-
     #[test]
     fn byte_slice() {
         let bytes: [u8; 6] = [0, 1, 2, 3, 4, 5];
 
-        assert_eq!(bytes.into_ram_bytes(), bytes);
+        assert_eq!(bytes.to_ram_bytes(), bytes);
     }
-
-    // #[test]
-    // fn string_slice() {
-    // let hello: &str = "Hello, World!";
-
-    // assert_eq!(RamBytes::to_ram_bytes(hello), hello.as_bytes());
-    // }
 
     #[test]
     fn number_struct() {
@@ -69,47 +59,69 @@ mod tests {
 
         let bytes = [10, 0, 128, 0];
 
-        assert_eq!(size.to_ram_bytes(), bytes);
+        assert_eq!(unsafe { size.as_ram_bytes() }, bytes);
     }
 
-    // TODO: Fix this test (string slices are weird for this kind of thing)
-    // #[test]
-    // fn struct_data() {
-    //     #[derive(Debug, Copy, Clone)]
-    //     struct Person {
-    //         first: &'static str,
-    //         surname: &'static str,
-    //         age: u8,
-    //     }
+    #[test]
+    fn struct_data() {
+        #[derive(Debug, Copy, Clone)]
+        struct Person {
+            first: [char; 8],
+            surname: [char; 6],
+            age: u8,
+        }
 
-    //     let person = Person {
-    //         first: "Juliette",
-    //         surname: "Cordor",
-    //         age: 19,
-    //     };
+        let person = Person {
+            first: ['J', 'u', 'l', 'i', 'e', 't', 't', 'e'],
+            surname: ['C', 'o', 'r', 'd', 'o', 'r'],
+            age: 19,
+        };
 
-    //     // Rust analyzer says the struct is 40 bytes wide
-    //     // The first 16 bytes are for the first name
-    //     //      The first name is 8 bytes, + 3 for the additional string info
-    //     //      That leaves 5 null bytes
-    //     // The second 16 bytes are for the last name
-    //     //      The first name is 6 bytes, + 3 for the additional string info
-    //     //      That leaves 7 null bytes
-    //     // The last byte is for the age
-    //     // The remainder is null
-    //     let person_bytes: Vec<u8> = {
-    //         let mut v = Vec::with_capacity(40);
+        // Rust analyzer says the struct is 60 bytes wide
+        // The first 32 bytes are for the first name
+        // The second 32 bytes are for the last name
+        // The last byte is for the age
+        // The last 3 bytes are null
+        let person_bytes: Vec<u8> = {
+            let mut v = Vec::with_capacity(40);
 
-    //         v.extend(person.first.as_bytes());
-    //         pad_with_null(&mut v, 5);
-    //         v.extend(person.surname.as_bytes());
-    //         pad_with_null(&mut v, 7);
-    //         v.push(person.age);
+            v.extend(
+                &person
+                    .first
+                    .iter()
+                    .flat_map(|c| (*c as u32).to_ne_bytes())
+                    .collect::<Vec<_>>(),
+            );
+            v.extend(
+                &person
+                    .surname
+                    .iter()
+                    .flat_map(|c| (*c as u32).to_ne_bytes())
+                    .collect::<Vec<_>>(),
+            );
+            v.push(person.age);
+            v.extend(&[0; 3]);
 
-    //         v
-    //     };
-    //     assert_eq!(into_bytes(person), person_bytes);
-    // }
+            v
+        };
+        assert_eq!(person.to_ram_bytes(), person_bytes);
+    }
+
+    #[test]
+    fn test_conversions() {
+        let data: i32 = 0b0010_0101_1010_0101_0010_0101_1010_0101;
+
+        let bytes = data.to_ram_bytes();
+
+        assert_eq!(bytes, data.to_le_bytes());
+
+        #[allow(clippy::cast_ptr_alignment)]
+        let ptr = Box::into_raw(bytes.into_boxed_slice()).cast::<i32>();
+
+        let decoded_data = unsafe { *ptr };
+
+        assert_eq!(data, decoded_data);
+    }
 
     // #[test]
     // fn packed_struct_data() {
